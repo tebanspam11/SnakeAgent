6027857d37e3125419173df0c0daeb74c4ca35a0
Esteban Barrera <111403759+tebanspam11@users.noreply.github.com>
2025-10-05 11:52:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+# Normal size board dimensions on Google Snake
 ROWS = 15
 COLUMNS = 17
 
-TILE_SIZE = 35  # in pixels
-TOP = 243
-LEFT = 339
+TILE_SIZE = 35  # Pixels size of each box, approx 35.4
+# Coordinates of top left box (Deafult -> Google Chrome dark theme, full screen)
+TOP = 310 # y coordinates 
+LEFT = 434 # x coordinates 
 
+# Time interval of snake moving box to box
 MOVE_INTERVAL = 0.135 
